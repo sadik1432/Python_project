b7d7aa1d7b9f7e5657fe4ec7274471fe64036f35
Abdus Sadik Molla <abdussadikmolla@gmail.com>
2022-03-16 22:27:12 +0530

    Add Area of Circle Code

---
+++ Area_of_Circle.py
@@ -0,0 +1,10 @@
+# Write a Python program to calculate area and perimeter of the circle.
+from math import pi 
+
+radius = float(input("Enter radius: "))
+# area of the circle
+print(f"Ther area of the circle with radius {radius} is {pi*radius**2} unit.\n---------------")
+
+# perimeter of the circle
+print(f"The perimeter of the circle with radius {radius} is {2*pi*radius}")
+print(f"The perimeter of the circle with radius {radius} is {2*pi*radius}")
